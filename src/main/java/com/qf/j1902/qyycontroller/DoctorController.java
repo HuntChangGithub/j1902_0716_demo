e14ac7d8fdb74221900a2c2289520b513ebbc094
qianyunyang <2997272453@qq.com>
2019-07-16 17:52:51 +0800

    qianyunyang-1

---
+++ src/main/java/com/qf/j1902/qyycontroller/DoctorController.java
@@ -0,0 +1,11 @@
+package com.qf.j1902.qyycontroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2019/7/16.
+ */
+@RestController
+public class DoctorController {
+    private int id;
+}
