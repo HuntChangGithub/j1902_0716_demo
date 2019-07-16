17660327cc8fb62585629b21418c12669fb04d8a
qianyunyang <2997272453@qq.com>
2019-07-16 18:53:50 +0800

    qianyunyang-2

---
+++ src/main/java/com/qf/j1902/qyycontroller/User.java
@@ -0,0 +1,10 @@
+package com.qf.j1902.qyycontroller;
+
+/**
+ * Created by Administrator on 2019/7/16.
+ */
+public class User {
+    private int id;
+    private String name;
+    private String dname;
+}
